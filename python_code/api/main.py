a43de010ba4ef8b92da709fad50c56acd2108b27
WuangZu <150925474+luuvu23092004@users.noreply.github.com>
2025-05-25 22:04:22 +0700

    Update main.py
    update 25/5

--- python_code/api/main.py
+++ python_code/api/main.py
@@ -3,8 +3,7 @@ import runpod
 
 def main():
     agent_controller = AgentController()
-    runpod.serverless.start({"handler": agent_controller.get_response})
-
+    runpod.serverless.start({"handler":agent_controller.get_response})
 
 if __name__ == "__main__":
     main()
