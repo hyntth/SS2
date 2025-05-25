1135ebbca0334ffbc47d6867c47c64e92bf95562
WuangZu <150925474+luuvu23092004@users.noreply.github.com>
2025-05-25 22:02:53 +0700

    Update utils.py
    update 25/5

--- python_code/api/agents/utils.py
+++ python_code/api/agents/utils.py
@@ -1,4 +1,4 @@
-def get_chatbot_response(client,model_name,messages,temperature=0):
+def get_chatbot_response(client, model_name, messages, temperature=0.0):
     input_messages = []
     for message in messages:
         input_messages.append({"role": message["role"], "content": message["content"]})
@@ -8,13 +8,13 @@ def get_chatbot_response(client,model_name,messages,temperature=0):
         messages=input_messages,
         temperature=temperature,
         top_p=0.8,
-        max_tokens=2000,
+        max_tokens=2000
     ).choices[0].message.content
-    
-    return response
 
-def get_embedding(embedding_client,model_name,text_input):
-    output = embedding_client.embeddings.create(input = text_input,model=model_name)
+    return response 
+
+def get_embedding(embedding_client, model_name, text_input):
+    output = embedding_client.embeddings.create(input = text_input, model=model_name)
     
     embedings = []
     for embedding_object in output.data:
@@ -22,17 +22,23 @@ def get_embedding(embedding_client,model_name,text_input):
 
     return embedings
 
-def double_check_json_output(client,model_name,json_string):
+
+def double_check_json_output(client, model_name, json_string):
     prompt = f""" You will check this json string and correct any mistakes that will make it invalid. Then you will return the corrected json string. Nothing else. 
     If the Json is correct just return it.
 
+    if there is any text before order after the json string, remove it.
     Do NOT return a single letter outside of the json string.
+    The first thing you write should be open curly brace of the json and the last letter you write should be the closing curly brace.
+
+    You should check the json string for the following text between triple backticks:
+    ```
 
     {json_string}
+
+    ```
     """
-
-    messages = [{"role": "user", "content": prompt}]
-
-    response = get_chatbot_response(client,model_name,messages)
-
+    messages = [{"role":'user', "content":prompt}]
+    response = get_chatbot_response(client, model_name, messages)
+    response = response.replace("`","")
     return response
